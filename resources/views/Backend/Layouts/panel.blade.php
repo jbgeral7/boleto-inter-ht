fa5ece69adbc186af5b137e0dd3c7f61b6240fc0
Leonardo Nascimento <leonardo.nascimento21@gmail.com>
2022-06-26 15:16:09 -0300

    Redirect panel after login

--- resources/views/Backend/Layouts/panel.blade.php
+++ resources/views/Backend/Layouts/panel.blade.php
@@ -259,6 +259,17 @@
               </p>
             </a>
           </li>
+          <li class="nav-item">
+            <a href="{{ route('logout') }}" class="nav-link" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+              <i class="fas fa-sign-out-alt"></i>
+              <p>
+                Sair
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                  {{ csrf_field() }}
+                </form>
+              </p>
+            </a>
+          </li>
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
