acfd4fe8c2518de0e2f8d992b9bb569aa7e64b10
Leonardo Nascimento <leonardo.nascimento21@gmail.com>
2022-08-14 14:43:34 -0300

    Adicionado valor dos serviços na lista dos clientes

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -3,3 +3,11 @@
 function convertBRL($number){
     return number_format($number, 2, ',', '.');
 }
+
+function checkCustomerValueService($customer){
+    $total_price = 0;
+    foreach($customer->services as $service){
+        $total_price += number_format($service->price, 2, '.', '');
+    }
+    return $total_price;
+}
